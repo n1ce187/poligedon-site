0a079389d6ec4ed73b4348efdd55096144559f8b
n1ce187 <bigman2004@windowslive.com>
2025-05-29 21:07:10 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './style.css';
+import './style1.css';
+import './style2.css';
+import './style3.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
